954fb08b54e1e33bb741694d04cf41b1dbd48ec1
Geoff Hing <geoffhing@gmail.com>
2016-03-10 05:58:32 -0600

    Break out loader instantiation into fixture

--- tests/test_chicago_result_loader.py
+++ tests/test_chicago_result_loader.py
@@ -7,19 +7,20 @@ DATA_DIRECTORY = os.path.join(os.path.dirname(os.path.abspath(__file__)),
         'data')
 
 class TestChicagoResultLoader(unittest.TestCase):
+    def setUp(self):
+        self._loader = ChicagoResultsLoader()
+        self._data_path = os.path.join(DATA_DIRECTORY, 'summary.txt')
+
     def test_load(self):
-        loader = ChicagoResultsLoader()
-        data_path = os.path.join(DATA_DIRECTORY, 'summary.txt')
-        results = loader.load(data_path)
+        results = self._loader.load(self._data_path)
         alvarez = next(r for r in results if r['candidate_name'] == "Anita Alvarez")
         self.assertEqual(alvarez['contest_code'], '0079')
         self.assertEqual(alvarez['candidate_number'], '002')
         self.assertEqual(alvarez['votes'], 0)
 
     def test_parse_result_full(self):
-        loader = ChicagoResultsLoader()
         line = "0079002206900000000000DEM       State's Attorney, Cook County                           Anita Alvarez                         Cook County              001"
-        result = loader.parse_result(line)
+        result = self._loader.parse_result(line)
         self.assertEqual(result['candidate_name'], "Anita Alvarez")
         self.assertEqual(result['contest_code'], '0079')
         self.assertEqual(result['candidate_number'], '002')
@@ -32,9 +33,8 @@ class TestChicagoResultLoader(unittest.TestCase):
         self.assertEqual(result['vote_for'], 1)
 
     def test_parse_result_numbers_only(self):    
-        loader = ChicagoResultsLoader()
         line = "0079002206900000000000"
-        result = loader.parse_result(line)
+        result = self._loader.parse_result(line)
         self.assertEqual(result['contest_code'], '0079')
         self.assertEqual(result['candidate_number'], '002')
         self.assertEqual(result['votes'], 0)
