ec6cb5630bdd59ab13e4cc69b54a9ac490c9c893
Geoff Hing <geoffhing@gmail.com>
2016-03-10 05:07:51 -0600

    Add excercise about refactoring for better testing

--- results/__init__.py
+++ results/__init__.py
@@ -1,3 +1,4 @@
+import codecs
 import json
 
 class SimpleResultLoader(object):
@@ -7,3 +8,52 @@ class SimpleResultLoader(object):
     def load(self, s):
         parsed = json.loads(s)
         return [self.handle_result(r) for r in parsed['results']]
+
+
+class ChicagoResultsLoader(object):
+    """
+    Parse Chicago Board of Elections Results File.
+
+    The file is in this format:
+
+    Description                   Length Column Position
+
+    Contest Code                  4      1-4
+    Candidate Number              3      5-7
+    # of Eligible Precincts       4      8-11
+    Votes                         7      12-18
+    # Completed precincts         4      19-22
+    Party Abbreviation            3      23-25
+    Political Subdivision Abbrev  7      26-32
+    Contest name                  56     33-88
+    Candidate Name                38     89-126
+    Politicalcal subdivision name 25     127-151
+    Vote For                      3      152-154
+
+    """
+    def load(self, path):
+        results = []
+        fields = [
+            ('contest_code', 0, 4, unicode),
+            ('candidate_number', 4, 3, unicode),
+            ('num_eligible_precincts', 7, 4, int),
+            ('votes', 11, 7, int),
+            ('num_completed_precincts', 18, 4, int),
+            ('party_abbreviation', 22, 3, unicode),
+            ('political_subdivision_abbreviation', 25, 7, unicode),
+            ('contest_name', 32, 56, unicode),
+            ('candidate_name', 88, 38, unicode),
+            ('political_subdivision_name', 126, 25, unicode),
+            ('vote_for', 151, 3, int),
+        ]
+        with codecs.open(path, 'r', 'utf-8') as f:
+            for line in f:
+                result = {}
+                for field_name, field_start, field_length, parser in fields:
+                    field_raw = line[field_start:field_start + field_length]
+                    field_raw = field_raw.strip()
+                    result[field_name] = parser(field_raw)
+
+                results.append(result)
+
+        return results
---
+++ tests/test_chicago_result_loader.py
@@ -0,0 +1,17 @@
+import os
+import unittest
+
+from results import ChicagoResultsLoader
+
+DATA_DIRECTORY = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+        'data')
+
+class TestChicagoResultLoader(unittest.TestCase):
+    def test_load(self):
+        loader = ChicagoResultsLoader()
+        data_path = os.path.join(DATA_DIRECTORY, 'summary.txt')
+        results = loader.load(data_path)
+        alvarez = next(r for r in results if r['candidate_name'] == "Anita Alvarez")
+        self.assertEqual(alvarez['contest_code'], '0079')
+        self.assertEqual(alvarez['candidate_number'], '002')
+        self.assertEqual(alvarez['votes'], 0)
