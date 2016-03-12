9bbc54528405bd10867cdf55ca44fa82dde2f371
Geoff Hing <geoffhing@gmail.com>
2016-03-12 09:15:51 -0700

    Add solution to debugging excercise

--- results/broken.py
+++ results/broken.py
@@ -40,7 +40,41 @@ class BrokenChicagoResultsLoader(object):
                 for field_name, field_start, field_length, parser in fields:
                     field_raw = line[field_start:field_start + field_length]
                     field_raw = field_raw.strip()
-                    result[field_name] = parser(field_raw)
+                    try:
+                        result[field_name] = parser(field_raw)
+                    except Exception:
+                        # From the output of unittest, we know the error
+                        # occurred with this call in the code.  Catch
+                        # the exception and start the debugger
+                        import sys
+                        import pdb
+                        exc_name, exc_value, exc_traceback = sys.exc_info()
+                        pdb.post_mortem(exc_traceback)
+                        # Once in the debugger, I'd use print() to look at some
+                        # of the variables in the frame:
+                        #
+                        # > /Users/ghing/Dropbox/nicar2016/nicar2016-python-testing-debugging-excercises/results/broken.py(44)load()
+                        # -> result[field_name] = parser(field_raw)
+                        # (Pdb) l
+                        #  44                             result[field_name] = parser(field_raw)
+                        #  45                         except Exception:
+                        #  46                             import sys
+                        #  47                             import pdb
+                        #  48                             exc_name, exc_value, exc_traceback = sys.exc_info()
+                        #  49  ->                         pdb.post_mortem(exc_traceback)
+                        #  50
+                        #  51                     results.append(result)
+                        #  52
+                        #  53             return results
+                        # [EOF]
+                        # (Pdb) print(field_raw)
+                        # Álvaro R. Obregón (Sanders)
+                        # (Pdb) print(line)
+                        # 0023012034800000000000DEM       Delegate, National Convention 4th DEM                   Álvaro R. Obregón (Sanders)           4th Congressional Distric005
+                        # 
+                        # It looks like the issue is that there are non-ASCII
+                        # characters in the data that we'll have to handle
+                        # somehow
 
                 results.append(result)
 
